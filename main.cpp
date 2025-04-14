9518a9040f34a1cb6037eba3eb4f7234383aa288
USER-NOT-FOUND-maker <iamdemariam@gmail.com>
2025-04-14 14:25:06 +0100

    i have NO idea what im doing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+class Coordinate{
+	public:
+		int x;
+		int y;
+};
+
+class Sprite{
+	public:
+		int XCoordinate;
+		int YCoordinate;
+		Coordinate TopCorner;
+		Coordinate BottomCorner;
+	
+	void move(Sprite Sprite, int NewX, int NewY){
+		Sprite.XCoordinate = NewX;
+		Sprite.YCoordinate = NewY;
+	}
+};
+
 int main()
 {
         return 0;
